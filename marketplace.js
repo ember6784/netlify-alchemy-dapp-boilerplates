9f88bb49961941a6a6b491f852314b4127238085
ember6784 <124015588+ember6784@users.noreply.github.com>
2023-12-01 11:31:52 +0200

    Update marketplace.js

--- marketplace.js
+++ marketplace.js
@@ -1,1 +1,13 @@
-
+const { execFile } = require('child_process');
+const marketplace = process.argv[2]; // get the marketplace argument from the command line
+if (marketplace && marketplace.match(/^[a-z0-9-]+$/i)) { // check if the marketplace argument is valid
+  execFile('npx', ['create-web3-dapp', marketplace], (error, stdout, stderr) => { // execute the npx command safely
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(stdout);
+    }
+  });
+} else {
+  console.error('Invalid marketplace argument');
+}
